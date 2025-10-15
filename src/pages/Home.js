e504fd55ded1704e4552227a110566eedbebf053
Eulalio Medina Barragan <36649331+lalomedina09@users.noreply.github.com>
2025-10-15 13:06:01 -0600

    ajustes orden de archivos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from '../components/layout/Header';
-import Hero from '../components/Hero';
-import Features from '../components/Features';
-import About from '../components/About';
-import Services from '../components/Services';
-import Process from '../components/Process';
-import Industries from '../components/Industries';
-import Products from '../components/Products';
-import Contact from '../components/Contact';
+import Hero from './Home/Hero';
+import Features from './Home/Features';
+import About from './Home/About';
+import Services from './Home/Services';
+import Process from './Home/Process';
+import Industries from './Home/Industries';
+import Products from './Home/Products';
+import Contact from './Home/Contact';
 
 const Home = () => {
     return (
